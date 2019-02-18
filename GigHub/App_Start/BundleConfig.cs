914779e4c69deaf3044164a190a429b5f217f968
stevenliu88 <liuleiwangyue@gmail.com>
2019-02-18 13:34:14 +1100

    Initial Commit

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace GigHub
 {
     public class BundleConfig
     {
-        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        // https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
